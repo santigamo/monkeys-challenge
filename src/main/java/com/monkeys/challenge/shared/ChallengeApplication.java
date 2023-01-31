b0fc1cd1a463b3f62b588d3a92d185d2489f8030
Santi <santi.gamo@gmail.com>
2023-01-31 18:04:23 +0100

    chore: move Main class to shared package

--- src/main/java/com/monkeys/challenge/shared/ChallengeApplication.java
+++ src/main/java/com/monkeys/challenge/shared/ChallengeApplication.java
@@ -1,9 +1,11 @@
-package com.monkeys.challenge;
+package com.monkeys.challenge.shared;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.monkeys.challenge")
 public class ChallengeApplication {
 
 	public static void main(String[] args) {
